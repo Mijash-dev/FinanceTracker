ca6fb1eec299d6a6e9598e14d58d84bb19608fa8
Mijash Gaunkuri <mijashgaunkuri@gmail.com>
2026-08-21 11:43:00 +0545

    feat: update AccountController

--- controllers/AccountController.cs
+++ controllers/AccountController.cs
@@ -19,22 +19,45 @@ public class AccountController : ControllerBase
         var UserId = 1;
         var accounts = await _service.GetAccountsByUserIdAsync(UserId);
         var response = accounts.Select(account => new ResponseAccountRequest(account.Id, account.Name, account.Balance));
-        return ok(response);
+        return Ok(response);
     }
     [HttpGet("{Id:int}")]
-    public async Task<ActionResult<List<ResponseAccountRequest>>> GetAccount(int Id)
+    public async Task<ActionResult<ResponseAccountRequest>> GetAccount(int Id)
     {
         var UserId = 1;
-        var account = awit _service.GetAccountAsync(UserId, Id);
+        var account = await _service.GetAccountAsync(UserId, Id);
         if (account == null)
         {
             return NotFound();
         }
-        var response = account.Select(account => new ResponseAccountRequest(account.Id, Account.Name, account.Balance));
-        return ok(response); 
+        var response = new ResponseAccountRequest(account.Id, account.Name, account.Balance);
+        return Ok(response); 
     }
     [HttpPost]
-    public async Task<ActionResult<List<ResponseAccountRequest>>> CreateAccount (CreateAccountRequest request)
-
+    public async Task<ActionResult<ResponseAccountRequest>> CreateAccount (CreateAccountRequest request)
+    {
+        var UserID = 1;
+        var account = await _service.CreateAccountAsync(UserID,request.Name, request.Balance);
+        var response = new ResponseAccountRequest(account.Id, account.Name, account.Balance);
+        return CreatedAtAction ( nameof(GetAccount),new {Id = account.Id}, response);
+    }
+    [HttpPut("{Id:int}")]
+    public async Task<ActionResult<ResponseAccountRequest>> UpdateAccount (UpdateAccountRequest request, int Id)
+    {
+        var UserID = 1;
+        var update = await _service.UpdateAccountAsync(UserID,Id,request.Name, request.Balance);
+        if (!update){ return NotFound(); }
+        var account = await _service.GetAccountAsync(UserID,Id);
+        if (account == null) { return NotFound(); }
+        var response = new ResponseAccountRequest(account.Id, account.Name, account.Balance);
+        return Ok(response);
+    }
+    [HttpDelete("{Id:int}")]
+    public async Task<ActionResult> DeleteAccount(int Id) {
+        var UserID = 1;
+        var Delete = await _service.DeleteAccountAsync(UserID, Id);
+        if (!Delete) { return NotFound(); }
+        return NoContent();
+    }
 }
 
